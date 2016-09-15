bf79a915715b2315d82673bfd9542e69aefbe978
Topi99 <nokoayzack@gmail.com>
2016-09-14 23:53:43 -0500

    Mejoramos algunos bugs del tempalte base

--- CasaRent/urls.py
+++ CasaRent/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from casas import urls as casasUrls
+from main import urls as mainUrls
 from django.views.static import serve
 from django.conf import settings
 from accounts import urls as Accounts_Urls
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^', include(mainUrls, namespace="main")),
     url(r'^casas/',include(casasUrls, namespace="casas")),
     url(r'^users/', include(Accounts_Urls, namespace="users")),
     url(
